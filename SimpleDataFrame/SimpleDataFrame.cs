842e66fa32e7a9b7717fd84803cde51001f78f6b
MBDT <127850950+dimension-zero@users.noreply.github.com>
2024-10-23 11:24:57 +0100

    Update SimpleDataFrame.cs

--- SimpleDataFrame/SimpleDataFrame.cs
+++ SimpleDataFrame/SimpleDataFrame.cs
@@ -4,7 +4,7 @@ using System.IO;
 using System.Text;
 using CsvHelper;
 
-namespace Dimension.Library.Structures.SimpleDataFrame;
+namespace Dimension.Data.SimpleDataFrame;
 
 public class SimpleDataFrame
 {
